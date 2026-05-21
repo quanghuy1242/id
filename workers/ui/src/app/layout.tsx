68318d3311dd528df04b5d7ba52506264b27cadb
Huy Nguyen <quanghuy1242@gmail.com>
2026-05-21 20:42:21 +0700

    Correct  layout css

--- workers/ui/src/app/layout.tsx
+++ workers/ui/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import type { ReactNode } from "react";
-import _styles from "./globals.css";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "id admin",
